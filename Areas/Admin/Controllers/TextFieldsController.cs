ee89ed9a18267ea0c2c5858743f800209f6b2401
AlionaGrigorieva <41583475+AlionaGrigorieva@users.noreply.github.com>
2022-01-19 19:02:15 +0200

    Menu

--- Areas/Admin/Controllers/TextFieldsController.cs
+++ Areas/Admin/Controllers/TextFieldsController.cs
@@ -1,34 +1,34 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using TravelCompany.Domain;
-//using TravelCompany.Domain.Entities;
-//using TravelCompany.Service;
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelCompany.Domain;
+using TravelCompany.Domain.Entities;
+using TravelCompany.Service;
 
-//namespace TravelCompany.Areas.Admin.Controllers
-//{
-//    [Area("Admin")]
-//    public class TextFieldsController : Controller
-//    {
-//        private readonly DataManager dataManager;
-//        public TextFieldsController(DataManager dataManager)
-//        {
-//            this.dataManager = dataManager;
-//        }
+namespace TravelCompany.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TextFieldsController : Controller
+    {
+        private readonly DataManager dataManager;
+        public TextFieldsController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
 
-//        public IActionResult Edit(string codeWord)
-//        {
-//            var entity = dataManager.TextFields.GetTextFieldByCodeWord(codeWord);
-//            return View(entity);
-//        }
+        public IActionResult Edit(string codeWord)
+        {
+            var entity = dataManager.TextFields.GetTextFieldByCodeWord(codeWord);
+            return View(entity);
+        }
 
-//        [HttpPost]
-//        public IActionResult Edit(TextField model)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                dataManager.TextFields.SaveTextField(model);
-//                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
-//            }
-//            return View(model);
-//        }
-//    }
-//}
+        [HttpPost]
+        public IActionResult Edit(TextField model)
+        {
+            if (ModelState.IsValid)
+            {
+                dataManager.TextFields.SaveTextField(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(model);
+        }
+    }
+}
