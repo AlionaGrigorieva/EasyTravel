050924bd38e48611bdfaca906d2a6eb5b60b4ed5
AlionaGrigorieva <41583475+AlionaGrigorieva@users.noreply.github.com>
2022-01-22 01:12:14 +0200

    Final changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TravelCompany.Domain;
 
 namespace TravelCompany.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
         }
     }
 }
---
+++ Models/ViewComponents/ServiceListViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelCompany.Domain;
+
+namespace TravelCompany.Models.ViewComponents
+{
+    public class ServiceListViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public ServiceListViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult) View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
